1a3aca4c6cf1a2a27ce1bb3282e6d52eac935117
Nathan Davies <hello@nathandavies.co.uk>
2015-04-22 20:57:40 +0100

    To string; as required by the Evaluator

--- src/Dust/Ast/InlineLiteral.hh
+++ src/Dust/Ast/InlineLiteral.hh
@@ -8,4 +8,16 @@ class InlineLiteral extends InlinePart
      * @var string
      */
     public string $value;
+
+    /**
+     * @return string
+     */
+    public function __toString(): string {
+        $str = "";
+        
+        if (!empty($this->parts))
+            foreach ($this->parts as $value) $str .= $value;
+
+        return "\"" . $str . "\"";
+    }
 }
